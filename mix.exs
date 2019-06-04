d6de7f0551038d5ad823006a839b6d9170d3455c
Jake Morrison <jake@cogini.com>
2019-06-04 08:36:11 +0800

    Sort deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MixDeployExample.MixProject do
     [
       app: :mix_deploy_example,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,19 +33,19 @@ defmodule MixDeployExample.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.1"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
+      {:distillery, "~> 2.0"},
       {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.0"},
-      {:mix_systemd, github: "cogini/mix_systemd", override: true},
       {:mix_deploy, github: "cogini/mix_deploy"},
+      {:mix_systemd, github: "cogini/mix_systemd", override: true},
+      {:phoenix, "~> 1.4.6"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
     ]
   end
 
