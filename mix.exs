f567460c6cf8ddb241985a75d45e0e71e51db003
Jake Morrison <jake@cogini.com>
2019-06-15 13:23:14 +0800

    Add TOML

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule MixDeployExample.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
+      {:toml, "~> 0.5.2"},
     ]
   end
 
