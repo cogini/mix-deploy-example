96e50c53b2a61c9964b8be3f1fb8d50ec98f916c
Jake Morrison <jake@cogini.com>
2019-06-15 09:31:39 +0800

    Add ssl to applications

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule MixDeployExample.MixProject do
   def application do
     [
       mod: {MixDeployExample.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
