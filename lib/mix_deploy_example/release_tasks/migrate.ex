e25f1a63dd7139403bca730ed888a828b2d5524a
Karol Moroz <k.j.moroz@gmail.com>
2019-07-04 13:45:41 +0200

    Maybe fix migrator

--- lib/mix_deploy_example/release_tasks/migrate.ex
+++ lib/mix_deploy_example/release_tasks/migrate.ex
@@ -16,40 +16,45 @@ defmodule MixDeployExample.ReleaseTasks.Migrate do
 
     config_exs = Path.join(config_dir, "config.exs")
 
-    app_env = case File.exists?(config_exs) do
-      true ->
-        IO.puts "==> Loading config file #{config_exs}"
-        Config.Reader.merge(app_env, Config.Reader.read!(config_exs))
+    app_env =
+      case File.exists?(config_exs) do
+        true ->
+          IO.puts("==> Loading config file #{config_exs}")
+          Config.Reader.merge(app_env, Config.Reader.read!(config_exs))
 
-      _ ->
-        app_env
-    end
+        _ ->
+          app_env
+      end
 
-    repo_config = Keyword.get(app_env, @app, @repo_module)
+    repo_config = Keyword.get(app_env, @app) |> Keyword.get(@repo_module)
     repo_config = Keyword.put(repo_config, :adapter, Ecto.Adapters.Postgres)
     Application.put_env(@app, @repo_module, repo_config)
 
     # Start requisite apps
-    IO.puts "==> Starting applications.."
+    IO.puts("==> Starting applications..")
+
     for app <- [:crypto, :ssl, :postgrex, :ecto, :ecto_sql] do
       {:ok, res} = Application.ensure_all_started(app)
-      IO.puts "==> Started #{app}: #{inspect res}"
+      IO.puts("==> Started #{app}: #{inspect(res)}")
     end
 
     # Start repo
-    IO.puts "==> Starting repo"
+    IO.puts("==> Starting repo")
     {:ok, _pid} = apply(@repo_module, :start_link, [[pool_size: 2, log: :info, log_sql: true]])
 
     # Run migrations for the repo
-    IO.puts "==> Running migrations"
+    IO.puts("==> Running migrations")
     priv_dir = Application.app_dir(@app, "priv")
     migrations_dir = Path.join([priv_dir, "repo", "migrations"])
 
     opts = [all: true]
     config = apply(@repo_module, :config, [])
     pool = config[:pool]
+
     if function_exported?(pool, :unboxed_run, 2) do
-      pool.unboxed_run(@repo_module, fn -> Ecto.Migrator.run(@repo_module, migrations_dir, :up, opts) end)
+      pool.unboxed_run(@repo_module, fn ->
+        Ecto.Migrator.run(@repo_module, migrations_dir, :up, opts)
+      end)
     else
       Ecto.Migrator.run(@repo_module, migrations_dir, :up, opts)
     end
