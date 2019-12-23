7c39eb3b2cefd5ac5f11c2e5b1c470efc111c6fc
Jake Morrison <jake@cogini.com>
2019-12-23 16:26:31 +0800

    Use service_type exec

--- config/prodaws.exs
+++ config/prodaws.exs
@@ -84,6 +84,7 @@ config :mix_systemd,
   release_system: :distillery,
   app_user: "app",
   app_group: "app",
+  service_type: :exec,
   distillery: true,
   env_vars: [
     "REPLACE_OS_VARS=true",
