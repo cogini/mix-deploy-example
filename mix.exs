9a14523b0266f9d87bbf5c938e909f5d60c0bea3
Jake Morrison <jake@cogini.com>
2019-12-24 12:39:06 +0800

    Sync config to /srv

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule MixDeployExample.MixProject do
       mix_deploy_example: [
         include_executables_for: [:unix],
         config_providers: [
-          {TomlConfigProvider, path: "/etc/mix-deploy-example/config.toml"}
+          # {TomlConfigProvider, path: "/etc/mix-deploy-example/config.toml"}
+          {TomlConfigProvider, path: "/srv/mix-deploy-example/config.toml"}
         ],
         steps: [:assemble, :tar]
       ],
